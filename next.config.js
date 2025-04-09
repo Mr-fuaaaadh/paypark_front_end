be05d7a03c2672ba9241d1b7c3f47143c11395bf
Muhammad Fuhad MK <muhammad.fuhad.dev@gmail.com>
2025-04-09 15:17:28 +0530

    fjnr

--- next.config.js
+++ next.config.js
@@ -6,14 +6,14 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http", // Change to https to avoid mixed content issues
+        protocol: "http",
         hostname: "13.60.216.198",
       },
     ],
   },
-  output: 'standalone',  // Ensures compatibility with Netlify (Serverless)
-  trailingSlash: true,   // Helps with route handling and avoids 404 errors
-  reactStrictMode: true, // Helps with development (strict checks)
+  output: 'standalone',
+  trailingSlash: true,  
+  reactStrictMode: true,
 
   webpack: (config, { isServer }) => {
     if (!isServer) {
